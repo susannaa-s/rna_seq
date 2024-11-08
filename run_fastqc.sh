e2563101551c3435a10389ade657176172dadb7b
Susanna Schaerer <susana.schaerer@unifr.ch>
2024-11-08 08:20:36 +0100

    small update on run_fasqc file

--- run_fastqc.sh
+++ run_fastqc.sh
@@ -3,13 +3,13 @@
 #SBATCH --partition=pibu_el8            # Specify the partition
 #SBATCH --output=fastqc_parallel_%j.out # Standard output log file
 #SBATCH --error=fastqc_parallel_%j.err  # Standard error log file
-#SBATCH --ntasks=1                      # Number of tasks (1 job)
+#SBATCH --ntasks=1                      # Number of tasks 
 #SBATCH --cpus-per-task=4               # Number of CPU cores to use
 #SBATCH --time=02:00:00                 # Time limit for the job
 #SBATCH --mem=16G                       # Total memory per job
 
 # Create an output directory for FastQC results if it doesn't exist
-mkdir -p /data/users/sschaerer/rnaseq_course/fastqc_results
+mkdir -p /data/users/sschaerer/fastqc_results
 
 # Define a function to run FastQC on a single file
 # $1 refers to the first argument passed to the function when called
@@ -18,8 +18,9 @@ run_fastqc() {
     # Print a message to track the file being processed
     echo "Running FastQC on $file" 
     apptainer exec --bind /data/courses/rnaseq_course/toxoplasma_de/reads:/data/courses/rnaseq_course/toxoplasma_de/reads \
-        /containers/apptainer/fastqc-0.12.1.sif fastqc -o /data/users/sschaerer/rnaseq_course/fastqc_results "$file"
+        /containers/apptainer/fastqc-0.12.1.sif fastqc -o /data/users/sschaerer/fastqc_results "$file"
 }
+# decided to ttsor ethe fastqc results outside of the repository directory since the amount of data blocks the git commit 
 # The --bind option explicitly maps the reads directory to ensure the container can access it
 
 # Export the function to make it accessible to GNU Parallel
